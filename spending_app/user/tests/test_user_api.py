2de0200e1148b590b522c25ff1d03e2c8b98fc88
arunas445 <skittlessxit@gmail.com>
2021-09-08 22:45:59 +0300

    Created a profile view, serializer, to check your user profile and update it

--- spending_app/user/tests/test_user_api.py
+++ spending_app/user/tests/test_user_api.py
@@ -1,12 +1,12 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-
 from rest_framework.test import APIClient
 from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
+PROFILE_URL = reverse('user:profile')
 User = get_user_model()
 
 
@@ -104,3 +104,45 @@ class PublicUserApiTests(TestCase):
         response = self.client.post(TOKEN_URL, payload)
         self.assertNotIn('token', response.data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unauthorized(self):
+        # Test that authentication is required for users
+        response = self.client.get(PROFILE_URL)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserApiTests(TestCase):
+    # Tests that require an authentication
+    def setUp(self):
+        self.user = create_user(
+            email='test@email.com', password='password123', name='test name')
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        # Test retrieving profile for logged in used
+        response = self.client.get(PROFILE_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {
+            'name': self.user.name,
+            'email': self.user.email
+        })
+
+    def test_post_profile_not_allowed(self):
+        # Test that POST is not allowed on the profile url
+        response = self.client.post(PROFILE_URL, {})
+        self.assertEqual(response.status_code,
+                         status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        # Test updating the user profile for authenticated user
+        payload = {
+            'name': 'testupdate',
+            'password': 'password12updated',
+        }
+        response = self.client.patch(PROFILE_URL, payload)
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- spending_app/user/views.py
+++ spending_app/user/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from .serializers import UserSerializer, AuthTokenSerializer
@@ -10,6 +10,17 @@ class CreateUserView(generics.CreateAPIView):
 
 
 class CreateTokenView(ObtainAuthToken):
-    # Create a new custon auth token for user
+    # Create a new custom auth token for user
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    # Manage the profile of the authenticated user
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        # Retrieve and return authenticated user
+        return self.request.user
